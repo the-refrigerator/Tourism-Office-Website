ae42708b6e20423f0c3b15d2cc440de5dd8851e9
the-refrigerator <65685646+the-refrigerator@users.noreply.github.com>
2023-10-07 14:53:30 +0300

    collected static files for deployment

--- TourismOfficeBackend/settings.py
+++ TourismOfficeBackend/settings.py
@@ -32,8 +32,6 @@ if str(os.getenv("production")) == "True":
 else:
     DEBUG = True
 
-print(DEBUG)
-
 if not DEBUG:
     # HTTPS Settings
     SESSION_COOKIE_SECURE = True
@@ -164,6 +162,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
+# STATIC_ROOT = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
