d0aa993ce19c1e50448b68154810901bf08cbb48
the-refrigerator <65685646+the-refrigerator@users.noreply.github.com>
2023-10-07 14:46:51 +0300

    added static file support

--- TourismOfficeBackend/settings.py
+++ TourismOfficeBackend/settings.py
@@ -32,6 +32,8 @@ if str(os.getenv("production")) == "True":
 else:
     DEBUG = True
 
+print(DEBUG)
+
 if not DEBUG:
     # HTTPS Settings
     SESSION_COOKIE_SECURE = True
@@ -74,6 +76,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -160,8 +163,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
