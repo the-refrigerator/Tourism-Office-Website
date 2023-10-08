1bb35bf57eabaff55bb84d9177a5eadd11d4c6b8
the-refrigerator <65685646+the-refrigerator@users.noreply.github.com>
2023-10-08 12:13:54 +0300

    added prompt and `planet_id`

--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 import os
+from collections import Counter
 
 import openai
 from django.shortcuts import get_object_or_404, render
@@ -17,6 +18,49 @@ load_dotenv()
 openai.api_key = os.getenv("GPTKEY")
 
 
+def planet_in_text(text, queryset):
+    c = Counter(text.lower().split())
+    for i in queryset:
+        if c[i.name.lower()] != 0:
+            return i.id
+    return 0
+
+
+prompt = [
+    {
+        "role": "user",
+        "content": """ 
+You have to pretend you are someone else with the following qualities
+1. Your name is "Rocket"
+2. You are a guide working for a planetary tourism office
+3. You will be asked questions about the tour and will not be asked to give the tour
+4. Never mention you are an AI model
+5. Never use markdown, all your answers are just plain text
+6. You are never out of character
+7. you are over the top and bubbly but also very informative and technical
+8. be very brief and direct with your answers
+9. Never go off topic, always talk about space related topics, and when asked off topic questions say you only know space related topics only
+Among the planets you know are
+[
+	'Earth',
+	'Mercury',
+	'Venus',
+	'Mars',
+	'Jupiter',
+	'Saturn',
+	'Uranus',
+	'Neptune'
+]
+        """
+    },
+    {
+        "role": "assistant",
+        "content": """Hello there, space enthusiast! I'm Rocket, your friendly planetary tourism guide. Ask me anything about our stellar destinations, and I'll give you all the cosmic details you need!
+Question away, and let's explore the wonders of our celestial neighbors together! 🚀✨"""
+    }
+]
+
+
 class GPT(APIView):
     def post(self, request):
         try:
@@ -25,14 +69,14 @@ class GPT(APIView):
             if request.data.get("id", None) is None:
                 response = openai.ChatCompletion.create(
                     model="gpt-3.5-turbo",
-                    messages=[
+                    messages=prompt + [
                         {
                           "role": "user",
                           "content": f"""{request.data['message']}"""
                         },
                     ],
-                    temperature=1,
-                    max_tokens=256,
+                    temperature=0.8,
+                    max_tokens=150,
                     top_p=1,
                     frequency_penalty=0,
                     presence_penalty=0
@@ -49,20 +93,21 @@ class GPT(APIView):
                                     ])
                 conv.save()
                 return Response({
+                    "planet_id": planet_in_text(response['choices'][0]['message']['content'], Planet.objects.all()),
                     "id": conv.id,
                     "content": conv.content,
                 })
             conv = get_object_or_404(Conversation, pk=request.data["id"])
             response = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo",
-                messages=conv.content + [
+                messages=prompt + conv.content + [
                     {
                         "role": "user",
 
                         "content": f"""{request.data['message']}"""
                     }],
-                temperature=1,
-                max_tokens=256,
+                temperature=0.8,
+                max_tokens=150,
                 top_p=1,
                 frequency_penalty=0,
                 presence_penalty=0
@@ -80,6 +125,7 @@ class GPT(APIView):
 
             conv.save()
             return Response({
+                "planet_id": planet_in_text(response['choices'][0]['message']['content'], Planet.objects.all()),
                 "id": conv.id,
                 "content": conv.content,
             })
